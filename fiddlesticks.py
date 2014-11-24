78460d664cc7d026124fd2c5709d4492025f4e75
morbidflight <morbidflight@gmail.com>
2014-11-23 21:06:06 -0600

    better version, still needs to be tinkered with

--- fiddlesticks.py
+++ fiddlesticks.py
@@ -1,18 +1,41 @@
+############################################
+# Fiddlesticks ult, a novel.
+# Generated text based on the feeling of 
+# playing fiddlesticks and ulting
+# because crowstorm.
+# by morbidflight for NaNoGenMo 2014
+# https://github.com/dariusk/NaNoGenMo-2014
+############################################ 
+
 import random
 
 word = "caw"
 endpunc = [".", "!", "?", "..."]
 midpunc = ["...", "-", ",", ":", ";"]
 
-nov = random.randint(1, 100)
+allpunc = endpunc + midpunc
 
-for x in range (0, nov):
-    temp = random.randint(1, 10)
-#    rtemp = random.randint(1, temp)
-    sent = [word.title()]
-    for y in range (0, temp):
-        sent.append(word)
-    print " ".join(sent),
-    print random.choice(endpunc),
+sent = []
 
+upper = True
+
+def addpunc(s):    # adds punctuation to the end of a word, maybe
+    result = s
+    if random.random() <= 0.1:
+        result += random.choice(allpunc)
+    result += " "
+    return result
+
+
+for x in range (0, 100):    # adds the capitalized form based on punc
+    if upper:
+        sent.append(addpunc(word).title())
+    else:
+        sent.append(addpunc(word))
+    if sent[x][3] in endpunc:
+        upper = True
+    else:
+        upper = False
+
+print "".join(sent),
 print "An enemy has been slain."
